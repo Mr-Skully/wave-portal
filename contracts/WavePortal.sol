d38d91bc2db41422215969275e347c2d7bdbb2ca
Mr-Skully <emiltitus.inc@gmail.com>
2021-09-13 23:19:46 +0530

    Add data storage functionality

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -7,13 +7,29 @@ import "hardhat/console.sol";
 contract WavePortal {
     uint totalWaves;
 
+    event NewWave(address indexed from, uint timestamp, string message);
+
+    struct Wave {
+        address waver;
+        uint timestamp;
+        string message;
+    }
+
+    Wave[] waves;
+
     constructor( ) {
         console.log("Hey, the WavePortal smart contract is up and running!!");
     }
 
-    function wave( ) public {
+    function wave(string memory _message) public {
         totalWaves += 1;
-        console.log("%s just waved!", msg.sender);
+        console.log("%s just waved, and recommended: %s", msg.sender, _message);
+        waves.push(Wave(msg.sender, block.timestamp, _message));
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    function getWaves( ) public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves( ) public view returns (uint) {
